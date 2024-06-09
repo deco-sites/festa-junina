a49113a3a9d2b2afe9438c6595eebd9cebf9c679
HenriqueeAl <henrifulls@gmail.com>
2024-06-09 18:30:40 -0300

    feat: contet and fix: header

--- sections/Content.tsx
+++ sections/Content.tsx
@@ -9,5 +9,24 @@ interface Props {
 }
 
 export default function Section({ title_One = "Festa", title_Two = "Junina", subtitle_One = "Celebre", subtitle_Two = "Viva São João, vamos festejar com alegria e amor!" }: Props) {
-  return <div>Hello {name}</div>
+  return (
+    <div class="w-full">
+      <div class="absolute w-96 h-28 bg-[#FF508A] rounded-r-3xl top-40"></div>
+      <div class="absolute w-28 h-32 bg-[#FFBA48] rounded-r-3xl top-56"></div>
+
+      <div class="absolute right-44 top-56">
+        <h1 class="text-9xl text-[#FF508A] font-primary text-right">
+            {title_One}
+            <p class="text-[#234E6E]">{title_Two}</p>
+        </h1>
+        <p class="text-[#FF508A] text-6xl font-primary text-right mt-4">{subtitle_One}</p>
+        <p class="text-[#234E6E] text-xl font-primary text-right mt-4">{subtitle_Two}</p>
+      </div>
+
+      <div class="absolute bottom-0 w-full flex justify-between">
+          <div class="w-2/5 h-28 bg-[#224D6C] rounded-tr-3xl"></div>
+          <div class="w-28 h-14 bg-[#00C2C9] rounded-tl-3xl absolute bottom-0 right-0"></div>
+      </div>
+    </div>
+  )
 }
--- sections/Header.tsx
+++ sections/Header.tsx
@@ -9,26 +9,26 @@ interface Props {
 }
 
 export default function Section({ name_One = "Home", name_Two = "Sobre", name_Three = "Serviços", name_Four="Contatos"}: Props) {
-  return (<header class="flex justify-between">
+  return (<header class="flex justify-between h-20 items-center pr-20 px-20">
       <nav>
         <ul class="flex gap-8">
-          <li class="text-[#224D6C] text-xl font-primary">
+          <li class="text-[#224D6C] text-xl font-primary cursor-pointer">
             {name_One}
           </li>
-          <li class="text-[#224D6C] text-xl font-primary">
+          <li class="text-[#224D6C] text-xl font-primary cursor-pointer">
             {name_Two}
           </li>
-          <li class="text-[#224D6C] text-xl font-primary">
+          <li class="text-[#224D6C] text-xl font-primary cursor-pointer">
             {name_Three}
           </li>
-          <li class="text-[#224D6C] text-xl font-primary">
+          <li class="text-[#224D6C] text-xl font-primary cursor-pointer">
             {name_Four}
           </li>
         </ul>
       </nav>
 
       <form>
-        <input type="text" />
+        <input type="text" placeholder="Pesquise" class="bg-[#224D6C] w-72 h-12 rounded-btn px-5 placeholder:text-white text-white outline-none"/>
       </form>
   </header>)
 }
