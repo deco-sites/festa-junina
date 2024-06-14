d28dbf888e9172ce743b06851ff4bc5eaf16501f
Henrique Almeida de Oliveira <henrique.almeida231@hotmail.com>
2024-06-14 03:38:19 +0000

    Release staging environment with 4 changes made by Henrique Almeida de Oliveira

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,24 +5,26 @@
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Banner.tsx";
-import * as $$$$$$1 from "./sections/Content.tsx";
-import * as $$$$$$2 from "./sections/Fixed.tsx";
-import * as $$$$$$3 from "./sections/Footer.tsx";
-import * as $$$$$$4 from "./sections/Header.tsx";
-import * as $$$$$$5 from "./sections/ItWorks.tsx";
-import * as $$$$$$6 from "./sections/Photos.tsx";
-import * as $$$$$$7 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$1 from "./sections/Banner2.tsx";
+import * as $$$$$$2 from "./sections/Content.tsx";
+import * as $$$$$$3 from "./sections/Fixed.tsx";
+import * as $$$$$$4 from "./sections/Footer.tsx";
+import * as $$$$$$5 from "./sections/Header.tsx";
+import * as $$$$$$6 from "./sections/ItWorks.tsx";
+import * as $$$$$$7 from "./sections/Photos.tsx";
+import * as $$$$$$8 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Banner.tsx": $$$$$$0,
-    "site/sections/Content.tsx": $$$$$$1,
-    "site/sections/Fixed.tsx": $$$$$$2,
-    "site/sections/Footer.tsx": $$$$$$3,
-    "site/sections/Header.tsx": $$$$$$4,
-    "site/sections/ItWorks.tsx": $$$$$$5,
-    "site/sections/Photos.tsx": $$$$$$6,
-    "site/sections/Theme/Theme.tsx": $$$$$$7,
+    "site/sections/Banner2.tsx": $$$$$$1,
+    "site/sections/Content.tsx": $$$$$$2,
+    "site/sections/Fixed.tsx": $$$$$$3,
+    "site/sections/Footer.tsx": $$$$$$4,
+    "site/sections/Header.tsx": $$$$$$5,
+    "site/sections/ItWorks.tsx": $$$$$$6,
+    "site/sections/Photos.tsx": $$$$$$7,
+    "site/sections/Theme/Theme.tsx": $$$$$$8,
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
